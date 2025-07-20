654249416996ef974fdaeca899aeecf01ce9350a
mati5076 <montecinosmatias142@gmail.com>
2025-07-20 02:29:01 -0300

    m

--- api_rest_c#/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ api_rest_c#/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211118e649395ef678399be696fc56cf8f606b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c7efe360a86f7d8a46f612dc4a8f44f7295aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
